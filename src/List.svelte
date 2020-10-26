9ebd6eb32cfc53c38994f8916046103143a55a87
Daarien <35134108+Daarien@users.noreply.github.com>
2020-10-26 17:47:10 +0300

    feat: add List for presentation purpose

---
+++ src/List.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let title: string;
+
+  type ListElement = {
+    order: number;
+    name: string;
+  };
+  const list: ListElement[] = [
+    { order: 1, name: "First" },
+    { order: 2, name: "Second" },
+  ];
+</script>
+
+<div class="list">
+  <h2 class="list-title">{title}</h2>
+  {#each list as { order, name }}
+    <p class="list-item">
+      <span class="item-order">{order}</span>
+      <span class="item-name">{name}</span>
+    </p>
+  {/each}
+</div>
+
+<style type="text/scss">
+  .list {
+    padding: 16px 24px;
+    border: 1px solid lightcoral;
+    .list-title {
+      color: lightcoral;
+    }
+    .list-item {
+      .item-order {
+        display: inline-flex;
+        font-weight: bold;
+        margin-right: 8px;
+      }
+    }
+  }
+</style>
